4a589b8f0cae8d72764d9fcc9f8ffe5a11990e08
HariMenon2002 <ultronhari7@gmail.com>
2024-10-18 17:16:54 +0530

    day 1 completed, currently working on sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,7 @@
 //copy the root layout first
 
+import Sidebar from "@/components/Sidebar";
+
 /*the thing is that some pages need a sidebar , that is why we created a seperate folder with pages for sidebar */
 
 
@@ -8,9 +10,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const  loggedIn={firstName:'Hari',lastName:'Menon'};
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
         {children}
     </main>
   );
